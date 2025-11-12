fb0a716bcaac9baa587fa3ba629edfd6a99b823e
Alex Ehlke <alex.ehlke@gmail.com>
2025-11-12 02:36:29 -0500

    wip

--- dist/tools/InfoTools.d.ts
+++
@@ -1,8 +0,0 @@
-import type { McpResult, OpenProjectCallback } from '../types/index.js';
-export declare class InfoTools {
-    static getWorkspaceInfo(projectPath: string, openProject: OpenProjectCallback): Promise<McpResult>;
-    static getProjects(projectPath: string, openProject: OpenProjectCallback): Promise<McpResult>;
-    static openFile(filePath: string, lineNumber?: number): Promise<McpResult>;
-}
-export default InfoTools;
-//# sourceMappingURL=InfoTools.d.ts.map
